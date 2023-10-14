db369e4aa25441ef3f4c8cd8eb64e3a8be56cbd7
bobAnthonyVarley <116462213+bobAnthonyVarley@users.noreply.github.com>
2023-10-14 18:58:03 +0100

    Delete testaccess.java

--- testaccess.java
+++
@@ -1,8 +0,0 @@
-import java.io.BufferedReader;
-
-public class x {
-	public static void testfile() {
-		BufferedReader br = new BufferedReader( new FileReader("fifthwebsiteattempt/testaccess.txt"));
-		System.out.print(br.readLine());
-	}
-}
